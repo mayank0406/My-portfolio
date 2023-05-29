3238ec3a74b146d5d3ffce148261d0531e63bbe7
mayank0406 <mayanksingh88882@gmail.com>
2023-05-29 14:45:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <>
+      <div id='main-div'>
+       
+      </div>
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='Footer'>
+        <div>
+      <h1>Made by : - @Mayank</h1>
+        </div>
+
+<div>
+    <li><a href="/"><i className="fa-brands fa-linkedin"></i></a></li>
+    <li><a href="/"><i className="fa-brands fa-twitter"></i></a></li>
+    <li><a href="/"><i className="fa-brands fa-github"></i></a></li>
+</div>
+    </div>
+  )
+}
+
+export default Footer
